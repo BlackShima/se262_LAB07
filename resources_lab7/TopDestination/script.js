8cc0dc1eb946d941a8266f19c4a12cac5da14736
BlackShima <Blackshima02@gmail.com>
2026-01-06 23:15:57 +0700

    Q1+Q2

---
+++ resources_lab7/TopDestination/script.js
@@ -0,0 +1,37 @@
+// 3.2.1 Destination Data [1 point]
+const destinations = [
+    
+];
+
+// Select the container div
+const container = document.querySelector("#destination-container");
+
+
+// 3.2.2 Create the table elements such the table headers [1 point]
+// 3.2.4 To style the table applying class "styled-table" [1 point]
+
+
+// 3.2.2 Add header text elements to header columns [2 points]
+
+
+
+
+// 3.2.3 Create the table body [4 points]
+    // Create table body elements and row elements [1 point]
+
+
+    // Create the index cell [1 point]
+
+
+    // Create the destination name cell [1 point]
+
+
+    // Create the description cell [1 point]
+
+
+   
+});
+table.appendChild(tbody);
+
+// Append the table to the container
+container.appendChild(table);
